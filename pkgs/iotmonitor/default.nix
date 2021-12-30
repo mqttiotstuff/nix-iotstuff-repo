a2f55be2fb243e15c55d4126a49de93b53ec025c
Patrice Freydiere <frett27@gmail.com>
2021-12-30 22:54:14 +0100

    version 0.2.5

--- pkgs/iotmonitor/default.nix
+++ pkgs/iotmonitor/default.nix
@@ -2,14 +2,14 @@
 
 stdenv.mkDerivation rec {
   pname = "iotmonitor";
-  version = "0.2.2";
+  version = "0.2.5";
 
   src = fetchgit {
     url = "https://github.com/mqttiotstuff/iotmonitor.git";
     # computed with nix-prefetch-git, using the fetch submodules
     sha256 = "1fp1yarqzpyaillmgxws2nck8zjfjk9qcgqrk3hndqaq1sy11wl2";
     fetchSubmodules = true;
-    rev = "ac20f2308d52ea12daa3c7e482b79d07451f664b";
+    rev = "342d07d0ec4635cfd2aa109e1dcfd5ab6534c5c7";
   };
 
   buildInputs = [ zig git cmake leveldb ];
