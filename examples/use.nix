8806e63ff3e4b9666293e8d53faf8224ad575db0
Patrice Freydiere <frett27@gmail.com>
2021-12-30 23:21:38 +0100

    update to 0.2.5 version,

--- examples/use.nix
+++ examples/use.nix
@@ -1,5 +1,5 @@
 { nixpkgs ? <nixpkgs>, iotstuff ? import (fetchTarball
-  "https://github.com/mqttiotstuff/nix-iotstuff-repo/archive/9b12720.tar.gz")
+  "https://github.com/mqttiotstuff/nix-iotstuff-repo/archive/d9a9fedeebbb5d7bb1bf402b01dd024d852be6d7.tar.gz")
   { } }:
 
 iotstuff.pkgs.mkShell rec { buildInputs = [ iotstuff.iotmonitor ]; }
